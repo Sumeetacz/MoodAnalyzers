db7d55ee1b2437082428c95725e1aa34904aba2c
Sumeetacz <sumeetacz@gmail.com>
2022-05-21 12:48:35 +0530

    Exception Invalid mood program

---
+++ MoodAnalyser/MoodAnalyserException.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace MoodAnalyserSpace
+{
+    [Serializable]
+    internal class MoodAnalyserException : Exception
+    {
+        private object nULL_MOOD;
+        private string v;
+
+        public MoodAnalyserException()
+        {
+        }
+
+        public MoodAnalyserException(string? message) : base(message)
+        {
+        }
+
+        public MoodAnalyserException(object nULL_MOOD, string v)
+        {
+            this.nULL_MOOD = nULL_MOOD;
+            this.v = v;
+        }
+
+        public MoodAnalyserException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected MoodAnalyserException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public static object ExceptionType { get; internal set; }
+    }
+}
